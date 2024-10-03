6c626f6213403ea4d21e3b6155faec03c9233c7c
jlb <JLB-P@users.noreply.github.com>
2024-10-02 23:42:07 -0600

    load021024

---
+++ SDK_Atmega328p/SDK_Atmega328p/Debug/makedep.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+adc.c
+
+comp.c
+
+i2c.c
+
+int0.c
+
+io_ports.c
+
+lcd_i2c.c
+
+leds.c
+
+main.c
+
+rtc_1307.c
+
